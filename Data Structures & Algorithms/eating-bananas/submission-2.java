cf2df4784840a3089bbe5bf01965740a6ab72239
kanak08 <54390508+kanak08@users.noreply.github.com>
2026-07-28 20:00:20 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int k = piles[0];
+        
+
+        for(int i=0;i<piles.length;i++){
+            k = Math.max(k, piles[i]);
+        }
+
+        if(h == piles.length){
+            return k;
+        }
+
+        int l = 0, r=k;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            long req_time = search(piles, mid);
+            if(req_time > h){
+                l = mid+1; 
+            }
+            if(req_time <= h){
+                r = mid-1;
+                k = Math.min(k,mid);
+
+            }
+        }
+        return k;
+        
+    }
+
+    public long search(int[] piles, int mid){
+        long sum=0;
+        for(int i=0;i<piles.length;i++){
+            sum+= Math.ceil((double)piles[i]/mid);
+        }
+        return sum;
+    }
+}
